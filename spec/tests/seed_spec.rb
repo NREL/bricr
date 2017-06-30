bec3504de1c100c08935609d8be8ef1d14934395
Dan Macumber <dan.macumber@gmail.com>
2017-06-30 11:35:53 -0600

    Adding stub tests for @nllong, adding stub SEED URL configuration in config.rb.in

---
+++ spec/tests/seed_spec.rb
@@ -0,0 +1,37 @@
+require_relative './../spec_helper'
+
+require 'fileutils'
+require 'parallel'
+
+describe 'BRICR' do
+
+  it 'should upload and download building sync for one cycle' do
+    xml_path = File.join(File.dirname(__FILE__), '../files/phase0/building_151.xml'
+    
+    # upload to SEED as new record
+    
+    # get building sync that we just uploaded, download and compare to uploaded one
+    
+    # pretend to run energy simulation, now we have building_151_results.xml
+    
+    xml_path = File.join(File.dirname(__FILE__), '../files/phase0/building_151.xml')
+    
+    # upload results for record, this is not really a new revision of the building, just adding results 
+
+  end
+  
+  it 'should upload and download building sync for multiple cycles' do
+    xml_path = File.join(File.dirname(__FILE__), '../files/phase0/building_151.xml'
+    
+    # upload to SEED as new record
+    
+    # now user has edited building sync and added info
+    
+    # DLM: we don't have phase 1 files, just reupload phase 0 but pretend user modified it
+    xml_path = File.join(File.dirname(__FILE__), '../files/phase0/building_151.xml'
+    
+    # upload new cycle
+
+  end
+  
+end
