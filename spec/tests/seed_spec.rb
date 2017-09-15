27305ffb27654b47a151211fb915343df1c0c6eb
Nicholas Long <nicholas.long@nrel.gov>
2017-09-15 02:12:38 -0600

    change response of uploadbuilding file to be propertyview (not propertystate)

--- spec/tests/seed_spec.rb
+++ spec/tests/seed_spec.rb
@@ -29,7 +29,8 @@ describe 'BRICR' do
 
     # get building sync that we just uploaded, download and compare to uploaded one
     expect(file[:status]).to eq 'success'
-    expect(file[:data][:property_state][:custom_id_1]).to eq UBID
+    # Note that the upload_buildingsync file now returns the property_view, not the property_state
+    expect(file[:data][:property_view][:state][:custom_id_1]).to eq UBID
 
     # pretend to run energy simulation, now we have building_151_results.xml
     puts 'running simulation'
