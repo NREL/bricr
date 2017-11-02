21bcb5a5ca1cf7363eb5687d0802ab2ac556f13f
Yixing Chen <yixingchen@lbl.gov>
2017-11-02 14:50:39 -0700

    add SIMULATE_BASELINE_ONLY in config to run baseline simulation only

--- bin/run_buildingsync.rb
+++ bin/run_buildingsync.rb
@@ -29,9 +29,12 @@ FileUtils.mkdir_p(out_dir)
 
 translator = BRICR::Translator.new(xml_path)
 translator.writeOSWs(out_dir)
-
 osw_files = []
-Dir.glob("#{out_dir}/**/*.osw") { |osw| osw_files << osw }
+if BRICR::SIMULATE_BASELINE_ONLY
+  osw_files.push "#{out_dir}/Baseline/in.osw"
+else
+  Dir.glob("#{out_dir}/**/*.osw") { |osw| osw_files << osw }
+end
 
 if BRICR::DO_SIMULATIONS
   num_sims = 0
