12d4172e66a1c1d6ed3ef0a5d4b907de6e33fdf8
Ainsworth <llin@llin-34052s.nrel.gov>
2019-09-19 14:25:47 -0600

    BuildingSync file modification for audit template; geojson to generate new format for audit template

---
+++ bin/buildingsync_at_modify.rb
@@ -0,0 +1,103 @@
+require 'rexml/document'
+require 'tempfile'
+
+# return line index of element
+def line_index(file, list)
+  tag = Hash.new
+
+  # find line number of <auc:Address> and </auc:Ownership>
+  File.foreach(file).with_index(1) do |line, index|
+    (0...list.size).each { |i|
+      if line.include?list[i]
+        tag[list[i]] = index
+      end
+    }
+  end
+  return tag
+end
+
+# modify a generated bs file:
+def modify_existing_bs_file()
+
+  outdir = './bs_output/'
+  if(File.exist?(outdir))
+    Dir.glob(File.join(outdir, "*.xml")).each do |file|
+      puts file
+      content = ""
+
+      # find line number of <auc:Address> and </auc:Ownership>
+      list = ["<auc:Address>", "</auc:Ownership>"]
+      index_list = line_index(file, list)
+      
+      addr_index = index_list["<auc:Address>"]
+      ownership_index = index_list["</auc:Ownership>"]
+      if(ownership_index == addr_index) 
+        puts "WARNING: unformatted BuildingSync file!"  
+        exit()
+      end
+
+      filename = file
+      remove = ownership_index - addr_index + 1
+      (addr_index..ownership_index).each {|line| content += File.readlines(file)[line - 1]
+      }
+
+      filename = file.split(outdir)[1]
+      id = filename.split('.xml')[0]
+
+      # remove lines from <auc:Address> to </auc:Ownership>
+      tmp = Tempfile.open(filename, outdir) do |fp|
+        File.foreach(file) do |line|
+          if $. >= addr_index and remove > 0
+            remove -= 1
+          else
+            fp.puts line
+          end
+        end
+        fp
+      end
+      FileUtils.copy(tmp.path, file)
+      tmp.unlink
+
+      # insert lines from <auc:Address> to </auc:Pwnership> before <auc:FloorsAboveGrade>
+      i_index = line_index(file, ["<auc:FloorsAboveGrade>"])["<auc:FloorsAboveGrade>"] - 1
+      tmp = Tempfile.open(filename, outdir) do |fp|
+        File.readlines(file).insert(i_index, content).each do |line|
+          fp.puts line
+        end
+        fp
+      end
+      FileUtils.copy(tmp.path, file)
+      tmp.unlink
+
+      # insert new properties before </auc:Report>
+      fields = "          <auc:LinkedPremisesOrSystem>
+          <auc:Building>
+            <auc:LinkedBuildingID IDref='Building#{id}'/>
+          </auc:Building>
+        </auc:LinkedPremisesOrSystem>
+        <auc:UserDefinedFields>
+          <auc:UserDefinedField>
+            <auc:FieldName>Audit Template Report Type</auc:FieldName>
+            <auc:FieldValue>San Francisco Report</auc:FieldValue>
+          </auc:UserDefinedField>
+        </auc:UserDefinedFields>"
+
+      l_index = line_index(file, ["</auc:Report>"])["</auc:Report>"] - 1 
+      tmp = Tempfile.open(filename, outdir) do |fp|
+        File.readlines(file).insert(l_index, fields).each do |line|
+          fp.puts line
+        end
+        fp
+      end
+      FileUtils.copy(tmp.path, file)
+      tmp.unlink
+
+      # standard xml for JSON
+      $doc = REXML::Document.new File.new(file)
+      $doc.context[:attribute_quote] = :quote
+      $doc.write(File.open(file, "w"))
+    end
+  end
+end
+
+modify_existing_bs_file()
