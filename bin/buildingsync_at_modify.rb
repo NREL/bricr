2aa22255979089fdad7808b7e97eb139d8f86600
lin <llin@nrel.gov>
2019-09-30 22:44:27 -0600

    Ugly code. Need cleanup; xpath add elements; CData to add report; changed start/endtimestamp to lower case 'stamp'

--- bin/buildingsync_at_modify.rb
+++ bin/buildingsync_at_modify.rb
@@ -7,11 +7,11 @@ def line_index(file, list)
 
   # find line number of <auc:Address> and </auc:Ownership>
   File.foreach(file).with_index(1) do |line, index|
-    (0...list.size).each { |i|
+    (0...list.size).each do |i|
       if line.include?list[i]
         tag[list[i]] = index
       end
-    }
+    end
   end
   return tag
 end
@@ -24,55 +24,47 @@ def modify_existing_bs_file()
     Dir.glob(File.join(outdir, "*.xml")).each do |file|
       puts file
       content = ""
-
-      # find line number of <auc:Address> and </auc:Ownership>
-      list = ["<auc:Address>", "</auc:Ownership>"]
-      index_list = line_index(file, list)
       
-      addr_index = index_list["<auc:Address>"]
-      ownership_index = index_list["</auc:Ownership>"]
-      if(ownership_index == addr_index) 
-        puts "WARNING: BuildingSync file missing important elements!"  
-        exit()
+      xml = REXML::Document.new File.new(file)
+      xml_addr = REXML::XPath.first(xml, "//auc:Address")
+      xml_czt = REXML::XPath.first(xml, "//auc:ClimateZoneType")
+      xml_wdsId = REXML::XPath.first(xml, "//auc:WeatherDataStationID")
+      xml_wsn = REXML::XPath.first(xml, "//auc:WeatherStationName")
+      xml_long = REXML::XPath.first(xml, "//auc:Longitude")
+      xml_lat = REXML::XPath.first(xml, "//auc:Latitude")
+      xml_owner = REXML::XPath.first(xml, "//auc:Ownership")
+      xml_pid = REXML::XPath.first(xml, "//auc:PremisesIdentifiers")
+
+      xml_pid.next_sibling = xml_addr
+      xml_addr = REXML::XPath.first(xml, "//auc:Address")
+      xml_addr.next_sibling = xml_czt
+      xml_czt = REXML::XPath.first(xml, "//auc:ClimateZoneType")
+      xml_czt.next_sibling = xml_wdsId
+      xml_wdsId = REXML::XPath.first(xml, "//auc:WeatherDataStationID")
+      xml_wdsId.next_sibling = xml_wsn
+      xml_wsn = REXML::XPath.first(xml, "//auc:WeatherStationName")
+      xml_wsn.next_sibling = xml_long
+      xml_long = REXML::XPath.first(xml, "//auc:Longitude")
+      xml_long.next_sibling = xml_lat
+      xml_lat = REXML::XPath.first(xml, "//auc:Latitude")
+      xml_lat.next_sibling = xml_owner
+      xml.write(File.open(file, 'w'))
+
+      # Remove all start and end timestamp. Standard xml for JSON
+      REXML::XPath.each(xml, "//auc:StartTimeStamp") do |e|
+        e.name = "auc:StartTimestamp"
       end
-
-      remove = ownership_index - addr_index + 1
-      (addr_index..ownership_index).each {|line| content += File.readlines(file)[line - 1]
-      }
-
-      filename = file.split(outdir)[1]
-      id = filename.split('.xml')[0]
-
-      # remove lines from <auc:Address> to </auc:Ownership>
-      tmp = Tempfile.open(filename, outdir) do |fp|
-        File.foreach(file) do |line|
-          if $. >= addr_index and remove > 0
-            remove -= 1
-          else
-            fp.puts line
-          end
-        end
-        fp
+      REXML::XPath.each(xml, "//auc:EndTimeStamp") do |e|
+        e.name = "auc:EndTimestamp"
       end
-      FileUtils.copy(tmp.path, file)
-      tmp.unlink
-
-      # insert lines from <auc:Address> to </auc:Pwnership> before <auc:FloorsAboveGrade>
-      i_index = line_index(file, ["<auc:FloorsAboveGrade>"])["<auc:FloorsAboveGrade>"] - 1
-      tmp = Tempfile.open(filename, outdir) do |fp|
-        File.readlines(file).insert(i_index, content).each do |line|
-          fp.puts line
-        end
-        fp
-      end
-      FileUtils.copy(tmp.path, file)
-      tmp.unlink
+      xml.context[:attribute_quote] = :quote
+      xml.write(File.open(file, "w"))
 
       # insert new properties before </auc:Report>
-      xml = REXML::Document.new File.new(file)
+      xml_fields = ""
       if (REXML::XPath.first(xml, "//auc:LinkedPremisesOrSystem")).nil?
         building_id = REXML::XPath.first(xml, "//auc:LinkedBuildingID").attributes["IDref"]
-        fields = "          <auc:LinkedPremisesOrSystem>
+        xml_fields = REXML::CData.new("<auc:LinkedPremisesOrSystem>
             <auc:Building>
             <auc:LinkedBuildingID IDref='#{building_id}'/>
             </auc:Building>
@@ -82,27 +74,16 @@ def modify_existing_bs_file()
               <auc:FieldName>Audit Template Report Type</auc:FieldName>
               <auc:FieldValue>San Francisco Report</auc:FieldValue>
             </auc:UserDefinedField>
-          </auc:UserDefinedFields>"
-
-        l_index = line_index(file, ["</auc:Report>"])["</auc:Report>"] - 1 
-        tmp = Tempfile.open(filename, outdir) do |fp|
-          File.readlines(file).insert(l_index, fields).each do |line|
-            fp.puts line
-          end
-          fp
-        end
-        FileUtils.copy(tmp.path, file)
-        tmp.unlink
+          </auc:UserDefinedFields>", raw = "true")
       end
-
-      # Remove all start and end timestamp. Standard xml for JSON
-      doc = REXML::Document.new File.new(file)
-      doc.elements.delete_all("//auc:StartTimeStamp")
-      doc.elements.delete_all("//auc:EndTimeStamp")
-      doc.context[:attribute_quote] = :quote
-      doc.write(File.open(file, "w"))
+      xml.write(File.open(file, 'w'))
+      REXML::XPath.first(xml, "//auc:Report").add_text(xml_fields.value)
+      xml.write(File.open(file, 'w'))
     end
   end
+
+  return
+  
 end
 
 modify_existing_bs_file()
